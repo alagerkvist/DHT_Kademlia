85d6f69bd4b987581d6b0d6f577bc27e7fec9c77
ramirobg94 <ramirobg94@gmail.com>
2017-09-12 01:20:54 +0200

    update gitignore

--- bucket.go
+++ bucket.go
@@ -15,20 +15,28 @@ func newBucket() *bucket {
 }
 
 func (bucket *bucket) AddContact(contact Contact) {
+	//create a element variable
 	var element *list.Element
+	//this is to iterate over a list
+	//list.front() returns the first element of a list
+
 	for e := bucket.list.Front(); e != nil; e = e.Next() {
 		nodeID := e.Value.(Contact).ID
 
 		if (contact).ID.Equals(nodeID) {
+			//if the element is on the list, linear search , we save this element on the element variable
 			element = e
 		}
 	}
 
+	//if the element doesn't exist
 	if element == nil {
 		if bucket.list.Len() < bucketSize {
+			// if the list it is not full we push the new contact at the front
 			bucket.list.PushFront(contact)
 		}
 	} else {
+		// is the element exists we move the element to the front
 		bucket.list.MoveToFront(element)
 	}
 }
