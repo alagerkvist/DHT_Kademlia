b4446e9306b965ee546f65e798f500d0af8fe05b
ramirobg94 <ramirobg94@gmail.com>
2017-09-12 02:09:49 +0200

    comment kademlia

--- kademlia.go
+++ kademlia.go
@@ -3,14 +3,20 @@ package d7024e
 type Kademlia struct {
 }
 
+//LookupContact is a method of KAdemlia to locate some Node
+//PArams target: it is the finded contact
 func (kademlia *Kademlia) LookupContact(target *Contact) {
 	// TODO Testing ssh
 }
 
+//LookupContact is a method of KAdemlia to locate some Data
+//PArams hash: it is the finded data with the 160 bits hash
 func (kademlia *Kademlia) LookupData(hash string) {
 	// TODO
 }
 
+//Store is the method of KAdemlia to Store data
+// Params: data array of Bytes.
 func (kademlia *Kademlia) Store(data []byte) {
 	// TODO
 }
--- kademliaid.go
+++ kademliaid.go
@@ -7,19 +7,25 @@ import (
 
 const IDLength = 20
 
+//KAdemliaId is an array  20 bytes, 160 bites
 type KademliaID [IDLength]byte
 
+//This functions returns a KademliaId
+// params: data string
+// hex.DecodeString(data) DecodeString returns the bytes represented by the hexadecimal string s.
+// "48656c6c6f20476f7068657221" => Hello Gopher!
 func NewKademliaID(data string) *KademliaID {
 	decoded, _ := hex.DecodeString(data)
 
 	newKademliaID := KademliaID{}
 	for i := 0; i < IDLength; i++ {
-		newKademliaID[i] = decoded[i]
+		newKademliaID[i] = decoded[i] //it returns the codes of the chars %!s(uint8=72) H
 	}
 
 	return &newKademliaID
 }
 
+//return an [160bits]array of random uint8=72
 func NewRandomKademliaID() *KademliaID {
 	newKademliaID := KademliaID{}
 	for i := 0; i < IDLength; i++ {
@@ -28,15 +34,20 @@ func NewRandomKademliaID() *KademliaID {
 	return &newKademliaID
 }
 
+//Less is a method of KAdemliaId that returns a boolean if this currentNodeId is less than the KAdemliaId passed as param
+//Params: KademliaID: other ID
 func (kademliaID KademliaID) Less(otherKademliaID *KademliaID) bool {
+	//This loop iterate over the two IDs if in some point they are different returns the comparation of CurrentNodeID[i] < PassedNodeID[i]
 	for i := 0; i < IDLength; i++ {
 		if kademliaID[i] != otherKademliaID[i] {
 			return kademliaID[i] < otherKademliaID[i]
 		}
 	}
+	//If both nodes are the same it returns false
 	return false
 }
 
+//Equal is a method of KAdemliaId that returns a boolean if this currentNodeId is equal than the KAdemliaId passed as param
 func (kademliaID KademliaID) Equals(otherKademliaID *KademliaID) bool {
 	for i := 0; i < IDLength; i++ {
 		if kademliaID[i] != otherKademliaID[i] {
@@ -46,6 +57,7 @@ func (kademliaID KademliaID) Equals(otherKademliaID *KademliaID) bool {
 	return true
 }
 
+//REVIEW
 func (kademliaID KademliaID) CalcDistance(target *KademliaID) *KademliaID {
 	result := KademliaID{}
 	for i := 0; i < IDLength; i++ {
@@ -54,6 +66,8 @@ func (kademliaID KademliaID) CalcDistance(target *KademliaID) *KademliaID {
 	return &result
 }
 
+// Srting is a method of KAdemliaId that returns the string of an Encoded KademliaID
+//hex.EncodeToString(src) Hello -> 48656c6c6f
 func (kademliaID *KademliaID) String() string {
 	return hex.EncodeToString(kademliaID[0:IDLength])
 }
