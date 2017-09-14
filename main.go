ce3be74820695a1f951fec105d56a0b788bff0a5
alagerkvist <lager.kvist@hotmail.com>
2017-09-14 20:29:23 +0200

    Added to protobuf so you can send contacts

--- main.go
+++ main.go
@@ -12,14 +12,38 @@ import (
 
 func main() {
 	//kademlia.Listen("127.0.0.1", 8080);
-	testWhat := kademlia.ProtocolPackage_PING
+	typeOfMessage := kademlia.ProtocolPackage_PING
+	var testingRep = make([][]byte, 3)
+	//testingRep[0] = make([]byte, 3)
+	testingRep[0] = []byte("Client!!!!!")
+	testingRep[1] = []byte("IPPP!!!!!")
+	testingRep[2] = []byte("PORT!!!!!")
+
+	//testingRep[1] = make([]byte, 3)
+	//testingRep[3] = []byte("Client1111")
+	/*
+	testingRep[1][1] = byte("IPPP1111")
+	testingRep[1][2] = byte("PORT1111")
+	testingRep[2] = make(chan []byte, 3)
+	testingRep[2][0] = byte("Client222")
+	testingRep[2][1] = byte("IPPP222")
+	testingRep[2][2] = byte("PORT222")
+	*/
 	//p := &testWhat
 	testProto := &kademlia.ProtocolPackage{
 		ClientID: []byte("String"),
 		Ip: proto.String("localhost"),
 		ListenPort: proto.Int32(1234),
-		MessageSent: &testWhat,
+		MessageSent: &typeOfMessage,
+		ContactsKNearest: testingRep,
+
+		//ContactsKNearest: testingRep,
+		//ContactsKNearest: []byte("Client:123 ip:111"),
+		//ContactsKNearest: []byte("Client:1234 ip:222"),
+		//Then we can add both findID and findValue, they are optional
 	}
+
+
 
 	data, err := proto.Marshal(testProto)
 	if err != nil {
