9735b4d8aef5106341621245a8fd9b1386e76a6a
alagerkvist <lager.kvist@hotmail.com>
2017-09-15 10:14:36 +0200

    Fixed so now you can send Contacts

--- main.go
+++ main.go
@@ -13,23 +13,23 @@ import (
 func main() {
 	//kademlia.Listen("127.0.0.1", 8080);
 	typeOfMessage := kademlia.ProtocolPackage_PING
-	var testingRep = make([][]byte, 3)
-	//testingRep[0] = make([]byte, 3)
-	testingRep[0] = []byte("Client!!!!!")
-	testingRep[1] = []byte("IPPP!!!!!")
-	testingRep[2] = []byte("PORT!!!!!")
 
-	//testingRep[1] = make([]byte, 3)
-	//testingRep[3] = []byte("Client1111")
-	/*
-	testingRep[1][1] = byte("IPPP1111")
-	testingRep[1][2] = byte("PORT1111")
-	testingRep[2] = make(chan []byte, 3)
-	testingRep[2][0] = byte("Client222")
-	testingRep[2][1] = byte("IPPP222")
-	testingRep[2][2] = byte("PORT222")
-	*/
-	//p := &testWhat
+	testingRep := make([]*kademlia.ProtocolPackage_ContactInfo, 0)
+	//testingRep := [3]kademlia.ProtocolPackage_ContactInfo{}
+	t := kademlia.ProtocolPackage_ContactInfo{
+		ContactID: []byte("Client!!!!!"),
+		Address: proto.String("localhost"),
+		Distance: []byte("FAR!"),
+	}
+
+	testingRep = append(testingRep, &t)
+
+	t = kademlia.ProtocolPackage_ContactInfo{
+		ContactID: []byte("Client11"),
+		Address: proto.String("localhost1"),
+		Distance: []byte("FAR1"),
+	}
+	testingRep = append(testingRep, &t)
 	testProto := &kademlia.ProtocolPackage{
 		ClientID: []byte("String"),
 		Ip: proto.String("localhost"),
