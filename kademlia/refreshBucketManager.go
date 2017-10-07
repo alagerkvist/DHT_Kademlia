f69bcc68a596ab5d6a0d5b57e7d25de38178b000
QuentinDel <quentindelooz@gmail.com>
2017-10-07 13:51:41 +0200

    Refresh Bucket Manager, initialization time fixed

--- kademlia/refreshBucketManager.go
+++ kademlia/refreshBucketManager.go
@@ -10,28 +10,28 @@ import (
 
 
 func (kademlia *Kademlia) StartRefreshManaging(){
-	var buck bucket
+	var buck *bucket
 
 	//Init date for all buckets
+
 	for i:=0 ; i < IDLength * 8 ; i++ {
-		buck = *kademlia.network.myRoutingTable.buckets[i]
+		buck = kademlia.network.myRoutingTable.buckets[i]
 		buck.lastTimeVisited = time.Now().Local()
 	}
 
 	for{
-		time.Sleep(1 * time.Second)
+		time.Sleep(60 * time.Second)
+
 		fmt.Println("Check for refresh buckets")
 		for i:=0 ; i < IDLength * 8 ; i++{
-			buck = *kademlia.network.myRoutingTable.buckets[i]
-			duration := time.Since(buck.lastTimeVisited)
-			fmt.Println(duration)
+			buck = kademlia.network.myRoutingTable.buckets[i]
 
-			if duration.Hours() > 1 && buck.list.Len() > 0{
+			if time.Since(buck.lastTimeVisited).Hours() > 1 && buck.list.Len() > 0{
 				random := rand.Intn(buck.list.Len())
 				for e := buck.list.Front() ; e != nil ; e = e.Next() {
 					if random == 0{
-						//fmt.Println("\nProcess refresh of bucket %d\n", i)
-						//kademlia.LookupContact(e.Value.(Contact).ID)
+						fmt.Println("\nProcess refresh of bucket %d\n", i)
+						kademlia.LookupContact(e.Value.(Contact).ID)
 						break
 					}
 					random--
